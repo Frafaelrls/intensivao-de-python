eee0734150eac6e9f73f95b21d5571e337bfcc1c
Frafaelrls <frafaelrls@gmail.com>
2022-03-28 20:18:45 -0300

    updating the description

--- Aula_1/main.py
+++ Aula_1/main.py
@@ -1,3 +1,22 @@
+"""
+Desafio:
+
+Diariamente o nosso sistema atualiza as vendas do dia anterior.
+O seu trabalho diário, como analista, é enviar um e-mail para a diretoria,
+assim que começar a trabalhar, com o faturamento e a quantidade de produtos
+vendidos no dia anterior.
+
+E-mail da diretoria: seugmail+diretoria@gmail.com
+
+Local onde o sistema disponibiliza as vendas do dia anterior:
+
+https:/drive.google.com/drive/folders/
+
+Para resolver isso, vamos usar o pyautogui, uma biblioteca de automação de
+comandos do mouse e do teclado.
+
+"""
+
 import time
 import glob
 import os
@@ -67,7 +86,7 @@ pyautogui.hotkey('ctrl', 'v')
 
 # Escrever assunto
 pyautogui.press('tab')
-assunto = 'Planilha com indicadores'
+assunto = 'Dados dos indicadores'
 pyperclip.copy(assunto)
 pyautogui.hotkey('ctrl', 'v')
 
