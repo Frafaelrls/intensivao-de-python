a99735c025b49679a9094223a321581ca8fb3487
Frafaelrls <frafaelrls@gmail.com>
2022-04-14 13:47:23 -0300

    adding column drop in database

--- Aula_2/main.py
+++ Aula_2/main.py
@@ -12,15 +12,22 @@ Isso representa uma perda de milhões para a empresa.
 O que a empresa precisa fazer para resolver isso?
 
 """
+"""Passo 1 _ Importar a base de dados """
 import pandas as pd
-# Passo 1 _ Importar a base de dados
 
 tabela = pd.read_csv('telecom_users.csv')
+
+
+"""Passo 2 _ Visualizar a base de dados entendendo as informações"""
+# Retirando uma coluna da tabela
+# axis=0 -> Linha
+# axis=1 -> Coluna
+tabela = tabela.drop('Unnamed: 0', axis=1)
 print(tabela)
 
-# Passo 2 _ Visualizar a base de dados entendendo as informações disponíveis
-# Passo 3 _ Tratamento da Base de dados
-# Passo 4 _ Analise inicial / Analise global
-# Passo 5 _ Analise detalhada (buscar causa ou solução)
+
+"""Passo 3 _ Tratamento da Base de dados """
+"""Passo 4 _ Analise inicial / Analise global"""
+"""Passo 5 _ Analise detalhada (buscar causa ou solução)"""
 
 
