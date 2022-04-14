30af1e799adc543298f98683a710e296dc56939e
Frafaelrls <frafaelrls@gmail.com>
2022-04-14 13:35:25 -0300

    fist commit alua_2

---
+++ Aula_2/main.py
@@ -0,0 +1,26 @@
+"""
+Desafio
+
+Você trabalha em uma empresa de telecomunicação e tem clientes de vários
+serviços diferentes, entre os principais: 'internet' e telefone.
+
+O problema é que, analisando o histórico dos clientes dos últimos anos, você
+percebeu que a empresa está com Churn de mais de 26% dos clientes.
+
+Isso representa uma perda de milhões para a empresa.
+
+O que a empresa precisa fazer para resolver isso?
+
+"""
+import pandas as pd
+# Passo 1 _ Importar a base de dados
+
+tabela = pd.read_csv('telecom_users.csv')
+print(tabela)
+
+# Passo 2 _ Visualizar a base de dados entendendo as informações disponíveis
+# Passo 3 _ Tratamento da Base de dados
+# Passo 4 _ Analise inicial / Analise global
+# Passo 5 _ Analise detalhada (buscar causa ou solução)
+
+
